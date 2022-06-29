67e3e98c2de2af9b068634edce29d2dda4079cc3
Robert Durica <r.durica@gmail.com>
2022-06-29 23:00:18 +0200

    Added: Gradient's

--- SwiftUiBootcamp/Views/GradientsView.swift
+++ SwiftUiBootcamp/Views/GradientsView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct GradientsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25)
+            .fill(
+//                Color("CustomColor")
+                LinearGradient(
+                    colors:[Color("CustomColor"), Color.blue],
+                    startPoint: .bottom,
+                    endPoint: .topLeading
+                )
+//                RadialGradient(
+//                    colors: [Color("CustomColor"), Color.blue],
+//                    center: .center,
+//                    startRadius: 10,
+//                    endRadius: 100
+//                )
+//                AngularGradient(
+//                    gradient: Gradient(colors: [Color("CustomColor"), Color.blue]),
+//                    center: .topLeading,
+//                    angle: .degrees(180)
+//                )
+            )
+            .frame(width: 300, height: 200)
     }
 }
 
