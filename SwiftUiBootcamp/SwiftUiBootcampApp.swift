c4c75f126cc7a186e8733e4d033792dc858a6799
Robert Durica <r.durica@gmail.com>
2022-06-29 14:13:30 +0200

    Initial Commit

---
+++ SwiftUiBootcamp/SwiftUiBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBootcampApp.swift
+//  SwiftUiBootcamp
+//
+//  Created by Robert Ďurica on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
