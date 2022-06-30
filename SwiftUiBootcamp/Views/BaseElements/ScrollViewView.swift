32e7c4fdb6c5d1414551fd5255d850513c6a456f
Robert Durica <r.durica@gmail.com>
2022-06-30 14:15:02 +0200

    Added: ScrollView

---
+++ SwiftUiBootcamp/Views/BaseElements/ScrollViewView.swift
@@ -0,0 +1,29 @@
+//
+//  ScrollViewView.swift
+//  SwiftUiBootcamp
+//
+//  Created by Robert Ďurica on 30.06.2022.
+//
+
+import SwiftUI
+
+struct ScrollViewView: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false, content: {
+            LazyVStack {
+                ForEach(0..<100) { _ in
+                    Rectangle()
+                        .frame(height: 100)
+                        .foregroundColor(Color(UIColor.systemGray3))
+                }
+                    
+            }
+        })
+    }
+}
+
+struct ScrollViewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewView()
+    }
+}
