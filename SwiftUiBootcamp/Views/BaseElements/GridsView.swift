15287506ca34f506dd5fb8dd6218479ecc7ec8fd
Robert Durica <r.durica@gmail.com>
2022-06-30 22:14:36 +0200

    Added: GridsView

---
+++ SwiftUiBootcamp/Views/BaseElements/GridsView.swift
@@ -0,0 +1,41 @@
+//
+//  GridsView.swift
+//  SwiftUiBootcamp
+//
+//  Created by Robert Ďurica on 30.06.2022.
+//
+
+import SwiftUI
+
+struct GridsView: View {
+    let columns: [GridItem] = [
+//        GridItem(.fixed(50), spacing: nil, alignment: nil),
+//        GridItem(.flexible(), spacing: nil, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+    ]
+
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, pinnedViews: [.sectionHeaders]) {
+                Section(header:
+                    Text("Header")
+                        .background(.red)
+                        .frame(maxWidth: .infinity)
+                    , content: {
+                        ForEach(0 ..< 50) { _ in
+                            Rectangle()
+                                .frame(height: 100)
+                        }
+                    })
+            }
+        }
+    }
+}
+
+struct GridsView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridsView()
+    }
+}
