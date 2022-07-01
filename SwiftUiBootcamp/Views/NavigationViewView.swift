9d3f70d2d7a3aa1e0b83495121811a9304caadc5
Robert Durica <r.durica@gmail.com>
2022-07-01 23:59:03 +0200

    NavigationView, NavigationLink, .toolbar

---
+++ SwiftUiBootcamp/Views/NavigationViewView.swift
@@ -0,0 +1,77 @@
+//
+//  NavigationViewView.swift
+//  SwiftUiBootcamp
+//
+//  Created by Robert Ďurica on 01.07.2022.
+//
+
+import SwiftUI
+
+struct NavigationViewView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                NavigationLink("Hello, World!") {
+                    MySecondView()
+                }
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+
+            .navigationTitle("My Items")
+//            .navigationBarTitleDisplayMode(.inline)
+//            .navigationBarHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigation) {
+                    HStack {
+                        NavigationLink("Go1") {
+                            MySecondView()
+                        }
+                        NavigationLink("Go2") {
+                            MySecondView()
+                        }
+                        NavigationLink("Go2") {
+                            MySecondView()
+                        }
+                    }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Press Me2") {
+                        print("Pressed")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct MySecondView: View {
+    @Environment(\.presentationMode) var presentationmode
+    var body: some View {
+        ZStack {
+            ScrollView {
+                NavigationLink("Show me something!") {
+                    SheetsView()
+                }
+
+                Spacer()
+
+                Button {
+                    presentationmode.wrappedValue.dismiss()
+                } label: {
+                    Text("Go back to home page")
+                }
+            }
+
+            .navigationTitle("Second")
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct NavigationViewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationViewView()
+    }
+}
