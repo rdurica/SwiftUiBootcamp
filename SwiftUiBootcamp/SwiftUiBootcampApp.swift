a86ef1e6943b175f700d7205dba01b97a85d92a6
Robert Durica <r.durica@gmail.com>
2022-07-14 23:27:29 +0200

    @AppStorage

--- SwiftUiBootcamp/SwiftUiBootcampApp.swift
+++ SwiftUiBootcamp/SwiftUiBootcampApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUiBootcampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppStorageView()
         }
     }
 }
---
+++ SwiftUiBootcamp/Views/AppStorageView.swift
@@ -0,0 +1,30 @@
+//
+//  AppStorageView.swift
+//  SwiftUiBootcamp
+//
+//  Created by Robert Ďurica on 14.07.2022.
+//
+
+import SwiftUI
+
+struct AppStorageView: View {
+    @AppStorage("name") var currentUserName: String?
+
+    var body: some View {
+        VStack(spacing: 20) {
+            if let name = currentUserName {
+                Text(name)
+            }
+
+            Button("Save") {
+                currentUserName = "Emily" // Automatically save to AppStorage
+            }
+        }
+    }
+}
+
+struct AppStorageView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppStorageView()
+    }
+}
